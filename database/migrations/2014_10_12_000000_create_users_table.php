874a3f0d694e3feafd77d0bac19f2b519e0305e6
Polgár Attila <polgar.attila@dk.akkszalezi.hu>
2023-01-17 15:31:57 +0100

    Telefonszám tipusa megváltoztatása integerről stringre

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('keresztnev', 30);
             $table->string('email')->unique();
             $table->string('jelszo');
-            $table->integer('telefonszam');
+            $table->string('telefonszam');
             $table->string('varos');
             $table->string('cim');
             $table->boolean('hirlevel');
@@ -30,11 +30,11 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        User::create(['vezeteknev'=>'Polgár', 'keresztnev'=>'Attila','email'=>'polgar.attila@gmail.com', 'jelszo'=> Hash::make('Aa123456@'), 'telefonszam'=> 06701234567, 'varos'=>'Pécel', 'cim'=>'Petőfi Sándor utca 13', 'hirlevel'=>false, 'jogosultsag'=>3]);
+        User::create(['vezeteknev'=>'Polgár', 'keresztnev'=>'Attila','email'=>'polgar.attila@gmail.com', 'jelszo'=> Hash::make('Aa123456@'), 'telefonszam'=> '06701234567', 'varos'=>'Pécel', 'cim'=>'Petőfi Sándor utca 13', 'hirlevel'=>false, 'jogosultsag'=>3]);
 
-        User::create(['vezeteknev'=>'Somoskői', 'keresztnev'=>'Gábor','email'=>'smskgbr@gmail.com', 'jelszo'=> Hash::make('Aa123456@'), 'telefonszam'=> 06701234561, 'varos'=>'Budapest', 'cim'=>'Kolompér utca 13', 'hirlevel'=>true, 'jogosultsag'=>1]);
+        User::create(['vezeteknev'=>'Somoskői', 'keresztnev'=>'Gábor','email'=>'smskgbr@gmail.com', 'jelszo'=> Hash::make('Aa123456@'), 'telefonszam'=> '06701234561', 'varos'=>'Budapest', 'cim'=>'Kolompér utca 13', 'hirlevel'=>true, 'jogosultsag'=>1]);
 
-        User::create(['vezeteknev'=>'Kovács', 'keresztnev'=>'Dániel','email'=>'kovacs.daniel@gmail.com', 'jelszo'=> Hash::make('Aa123456@'), 'telefonszam'=> 06701234562, 'varos'=>'Budapest', 'cim'=>'Kolompér utca 14', 'hirlevel'=>true, 'jogosultsag'=>2]);
+        User::create(['vezeteknev'=>'Kovács', 'keresztnev'=>'Dániel','email'=>'kovacs.daniel@gmail.com', 'jelszo'=> Hash::make('Aa123456@'), 'telefonszam'=> '06701234562', 'varos'=>'Budapest', 'cim'=>'Kolompér utca 14', 'hirlevel'=>true, 'jogosultsag'=>2]);
     }
 
     /**
