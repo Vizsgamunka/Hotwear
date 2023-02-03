13058ae27e1f8b58f6176be54591360f465a13eb
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2023-02-03 17:58:03 +0100

    Controllerek létrehozása
    Minden jelenlegi file rendelkezik eg route-olt controllerrel.
    
    A user tábla átírás alatt van, ezért 2 controllerét commenteltem

--- app/Http/Controllers/TermekController.php
+++ app/Http/Controllers/TermekController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Termek;
 use Illuminate\Http\Request;
 
 class TermekController extends Controller
 {
-    //
+    public function index()
+    {
+        $termeks =  Termek::all();
+        return $termeks;
+    }
+
+    public function show($id)
+    {
+        $termeks = Termek::find($id);
+        return $termeks;
+    }
+
+    public function destroy($id)
+    {
+        Termek::find($id)->delete();
+    }
+    public function store(Request $request)
+    {
+        $termeks  = new Termek();
+        $termeks ->modell = $request->modell;
+        $termeks ->meret = $request->meret;
+        $termeks ->ar = $request->ar;
+        $termeks ->save();
+    }
+    public function update(Request $request, $id)
+    {
+        $termeks = Termek::find($id);
+        $termeks ->modell = $request->modell;
+        $termeks ->meret = $request->meret;
+        $termeks ->ar = $request->ar;
+        $termeks ->save();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\KategoriaController;
+use App\Http\Controllers\KepController;
+use App\Http\Controllers\ModellController;
+use App\Http\Controllers\Rend_tetelController;
+use App\Http\Controllers\RendelesController;
+use App\Http\Controllers\TermekController;
 use App\Http\Controllers\TervezoController;
 use App\Http\Controllers\UserController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -36,12 +40,63 @@ Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])-
     })->name('dashboard');
 });
 
-Route::get('/api/felhasznalok', [UserController::class, 'index']);
-Route::get('/api/felhasznalo_keres/{id}', [UserController::class, 'show']);
-Route::delete('/api/felhasznalo_torol/{id}', [UserController::class, 'destroy']);
-Route::get('/api/tervezok', [TervezoController::class, 'index']);
-Route::get('/api/tervezo_keres/{id}', [TervezoController::class, 'show']);
-Route::get('/api/tervezo_torol/{id}', [TervezoController::class, 'destroy']);
 Route::get('/api/kategoriak', [KategoriaController::class, 'index']);
 Route::get('/api/kategoria_keres/{id}', [KategoriaController::class, 'show']);
-Route::get('/api/kategoria_torol/{id}', [KategoriaController::class, 'destroy']);
+Route::post('/api/kategoria_tarol', [KategoriaController::class, 'store']);
+Route::put('/api/kategoria_frissit/{id}', [KategoriaController::class, 'update']);
+Route::delete('/api/kategoria_torol/{id}', [KategoriaController::class, 'destroy']);
+
+Route::get('/api/kepek', [KepController::class, 'index']);
+Route::get('/api/kep_keres/{id}', [KepController::class, 'show']);
+Route::post('/api/kep_tarol', [KepController::class, 'store']);
+Route::put('/api/kep_frissit/{id}', [KepController::class, 'update']);
+Route::delete('/api/kep_torol/{id}', [KepController::class, 'destroy']);
+
+Route::get('/api/modellek', [ModellController::class, 'index']);
+Route::get('/api/modell_keres/{id}', [ModellController::class, 'show']);
+Route::post('/api/modell_tarol', [ModellController::class, 'store']);
+Route::put('/api/modell_frissit/{id}', [ModellController::class, 'update']);
+Route::delete('/api/modell_torol/{id}', [ModellController::class, 'destroy']);
+
+Route::get('/api/rend_tetelek', [Rend_tetelController::class, 'index']);
+Route::get('/api/rend_tetels_keres/{id}', [Rend_tetelController::class, 'show']);
+Route::post('/api/rend_tetels_tarol', [Rend_tetelController::class, 'store']);
+Route::put('/api/rend_tetels_frissit/{id}', [Rend_tetelController::class, 'update']);
+Route::delete('/api/rend_tetels_torol/{id}', [Rend_tetelController::class, 'destroy']);
+
+Route::get('/api/rendelesek', [RendelesController::class, 'index']);
+Route::get('/api/rendeles_keres/{id}', [RendelesController::class, 'show']);
+Route::post('/api/rendeles_tarol', [RendelesController::class, 'store']);
+Route::put('/api/rendeles_frissit/{id}', [RendelesController::class, 'update']);
+Route::delete('/api/rendeles_torol/{id}', [RendelesController::class, 'destroy']);
+
+Route::get('/api/termekek', [TermekController::class, 'index']);
+Route::get('/api/termeks_keres/{id}', [TermekController::class, 'show']);
+Route::post('/api/termeks_tarol', [TermekController::class, 'store']);
+Route::put('/api/termeks_frissit/{id}', [TermekController::class, 'update']);
+Route::delete('/api/termeks_torol/{id}', [TermekController::class, 'destroy']);
+
+Route::get('/api/tervezok', [TervezoController::class, 'index']);
+Route::get('/api/tervezo_keres/{id}', [TervezoController::class, 'show']);
+Route::post('/api/tervezo_tarol', [TervezoController::class, 'store']);
+Route::put('/api/tervezo_frissit/{id}', [TervezoController::class, 'update']);
+Route::delete('/api/tervezo_torol/{id}', [TervezoController::class, 'destroy']);
+
+Route::get('/api/felhasznalok', [UserController::class, 'index']);
+Route::get('/api/felhasznalo_keres/{id}', [UserController::class, 'show']);
+Route::post('/api/felhasznalo_tarol', [UserController::class, 'store']);
+Route::put('/api/felhasznalo_frissit/{id}', [UserController::class, 'update']);
+Route::delete('/api/felhasznalo_torol/{id}', [UserController::class, 'destroy']);
+
+
+
+
+
+/*
+Route::get('/api/tervezok', [TervezoController::class, 'index']);
+Route::get('/api/tervezo_keres/{id}', [TervezoController::class, 'show']);
+Route::delete('/api/tervezo_torol/{id}', [TervezoController::class, 'destroy']);
+Route::get('/api/kategoriak', [KategoriaController::class, 'index']);
+Route::get('/api/kategoria_keres/{id}', [KategoriaController::class, 'show']);
+Route::delete('/api/kategoria_torol/{id}', [KategoriaController::class, 'destroy']);
+*/
